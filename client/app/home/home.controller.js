199bf5ee749b762921e35172f047088f0cc414ce
Pascoal Gomes <pascoal@mfdc.biz>
2017-02-12 18:44:38 +1000

    single digit calculation successful

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -12,50 +12,89 @@
 		home.selectNumber = selectNumber;
 		home.processOperation = processOperation;
 		home.getResult = getResult;
+		home.reset = reset;
 
 		init();
 
-		function init () {
+		function init() {
 			home.calc = {
 				num: {
-					selected: 0
+					selected: ''
 				},
 				operations: {
-					add: {
-						nums: []
-					},
-					subtract: {
-						nums: []
-					},
-					multiply: {
-						nums: []
-					},
-					divide: {
-						nums: []
-					}
+					add: [],
+					subtract: [],
+					multiply: [],
+					divide: []
 				},
 				current_operation: '',
 				result: 0
-				
+
 			}
 		}
-		
-		function selectNumber (num) {
-			home.calc.num.selected = num;
+
+		function selectNumber(num) {
+			home.calc.num.selected = home.calc.num.selected + num;
 		}
 
-		function processOperation (operator) {
+		function processOperation(operator) {
 
 			switch (operator) {
-				case '÷':
-					home.calc.operations.divide.push(home.calc.num.selected);
+				case 'divide':
+					home.calc.operations.divide.push(parseInt(home.calc.num.selected));
 					home.calc.current_operation = operator;
+					if (home.calc.operations.divide.length === 2) {
+						calculation(home.calc.operations.divide, operator);
+						home.calc.operations.divide = [];
+					}
+					break;
+
+				case 'multiply':
+					home.calc.operations.multiply.push(parseInt(home.calc.num.selected));
+					home.calc.current_operation = operator;
+					if (home.calc.operations.multiply.length === 2) {
+						calculation(home.calc.operations.multiply, operator);
+						home.calc.operations.multiply = [];
+					}
+					break;
+
+				case 'subtract':
+					home.calc.operations.subtract.push(parseInt(home.calc.num.selected));
+					home.calc.current_operation = operator;
+					if (home.calc.operations.subtract.length === 2) {
+						calculation(home.calc.operations.subtract, operator);
+						home.calc.operations.subtract = [];
+					}
+					break;
+
+				case 'add':
+					home.calc.operations.add.push(parseInt(home.calc.num.selected));
+					home.calc.current_operation = operator;
+					if (home.calc.operations.add.length === 2) {
+						calculation(home.calc.operations.add, operator);
+						home.calc.operations.add = [];
+					}
 					break;
 			}
 		}
 
-		function getResult () {
-				processOperation()
+		function calculation(arr, operation) {
+			OperationsFactory.getResult(arr, operation).then(function (response) {
+				if (response.status === 200) {
+					home.calc.result = response.data.data.result;
+				} else {
+					throw new Error('Invalid response!');
+				}
+			});
+		}
+
+		function getResult() {
+			processOperation(home.calc.current_operation);
+			home.calc.num.selected = '';
+		}
+
+		function reset() {
+			init();
 		}
 
 		return home;
--- client/app/operations/operations.factory.js
+++ client/app/operations/operations.factory.js
@@ -12,10 +12,7 @@
 		var resource = ApiFactory;
 
 		return {
-			getAdditionResult: getAdditionResult,
-			getSubtractionResult: getSubtractionResult,
-			getMultiplicationResult: getMultiplicationResult,
-			getDivisionResult: getDivisionResult
+			getResult: getResult
 		};
 
 		/**
@@ -23,44 +20,8 @@
 		 * @param {Array} nums
 		 * @returns Promise
 		 */
-		function getAdditionResult (nums) {
-			resource.setPath('add');
-			return resource.get(nums).then(function (response) {
-				return response;
-			});
-		}
-
-		/**
-		 * Return the subtraction result of two numbers
-		 * @param {Array} nums
-		 * @returns Promise
-		 */
-		function getSubtractionResult (nums) {
-			resource.setPath('subtract');
-			return resource.get(nums).then(function (response) {
-				return response;
-			});
-		}
-
-		/**
-		 * Return the multiplication result of two numbers
-		 * @param {Array} nums
-		 * @returns Promise
-		 */
-		function getMultiplicationResult (nums) {
-			resource.setPath('multiply');
-			return resource.get(nums).then(function (response) {
-				return response;
-			});
-		}
-
-		/**
-		 * Return the division result of two numbers
-		 * @param {Array} nums
-		 * @returns Promise
-		 */
-		function getDivisionResult (nums) {
-			resource.setPath('divide');
+		function getResult (nums, operation) {
+			resource.setPath(operation);
 			return resource.get(nums).then(function (response) {
 				return response;
 			});
